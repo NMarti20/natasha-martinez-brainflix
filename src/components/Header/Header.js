0e476f555344c53b78f226dcb0a0ab3c9e3ca94d
Natasha Martinez <natasha.martinezsj@gmail.com>
2021-08-24 17:07:59 -0700

    linked upload button to upload page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
-import HeaderScss from "./_header.scss";
+import "./_header.scss";
 import BrainflixLogo from "../../assets/images/logo/Logo-brainflix.svg";
 import ProfileImg from "../../assets/images/Mohan-muruge.jpg";
+import { Link } from "react-router-dom";
+import Upload from "../../pages/Upload";
 
 const Header = () => (
   <header className="header">
     <nav className="nav">
-      <img className="nav__logo" src={BrainflixLogo} alt="brainflix logo" />
-
+      <Link to="/" className="nav__logo-link">
+        <img className="nav__logo" src={BrainflixLogo} alt="brainflix logo" />
+      </Link>
       <div className="nav__container">
         <input
           className="nav__search-bar"
@@ -17,11 +20,14 @@ const Header = () => (
         ></input>
 
         <div className="nav__upload-and-img-container">
-          <div className="nav__upload">
-            <button type="button" className="nav__btn">
-              UPLOAD
-            </button>
-          </div>
+          <Link to="/upload">
+            <div className="nav__upload">
+              <button type="button" className="nav__btn">
+                <img className="nav__upload-fake" src={Upload} alt=""></img>
+                UPLOAD
+              </button>
+            </div>
+          </Link>
 
           <img
             className="nav__profile-img"
